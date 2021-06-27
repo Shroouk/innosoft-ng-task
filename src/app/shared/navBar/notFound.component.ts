682fd56ccceff9b7ae49c9d782898d1321b0bfae
Shrouk Saadon <69605056+Shroouk@users.noreply.github.com>
2021-06-27 17:11:10 +0200

    add source code

---
+++ src/app/shared/navBar/notFound.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'notFound',
+  templateUrl: './notFound.component.html',
+  styleUrls: ['./notFound.component.css'],
+})
+export class PageNotFoundComponent implements OnInit {
+
+
+  ngOnInit(): void {
+  }
+
+}
