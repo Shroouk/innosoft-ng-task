ab5ad78ecde9bb0d38ea9aafffe158a452b8b71a
Shrouk Saadon <69605056+Shroouk@users.noreply.github.com>
2021-06-27 17:13:42 +0200

    add source code

---
+++ src/app/shared/navBar/navBar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navBar',
+  templateUrl: './navBar.component.html',
+  styleUrls: ['./navBar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
